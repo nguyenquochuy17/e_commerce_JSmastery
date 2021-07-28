91e3ee7db047d9d6076a3e9523e1ca5592852d5a
nguyenquochuy <s3818764@rmit.edu.vn>
2021-07-28 16:54:42 +0700

    Product

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+import Product from './Product/Product'
+const products = [
+    { id: 1, name: " Shoes", description: "Running shoes", price: '$5', image: 'https://friendlyshoes.com/wp-content/uploads/Friendly-Shoes-Womens-Voyage-Low-Top-Mint-Peach-Shoe-Featured.jpg' },
+    { id: 2, name: "Macbook", description: "Apple Macbook", price: '$5', image: 'https://hc.com.vn/i/ecommerce/media/GS.007152_FEATURE_74775.jpg' },
+]
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
