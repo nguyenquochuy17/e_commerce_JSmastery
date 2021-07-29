312830916b811a47ddc9a91fce4ac68a8fed37b3
nguyenquochuy <s3818764@rmit.edu.vn>
2021-07-29 15:45:39 +0700

    Add to Cart

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -6,7 +6,7 @@ const products = [
     { id: 1, name: " Shoes", description: "Running shoes", price: '$5', image: 'https://friendlyshoes.com/wp-content/uploads/Friendly-Shoes-Womens-Voyage-Low-Top-Mint-Peach-Shoe-Featured.jpg' },
     { id: 2, name: "Macbook", description: "Apple Macbook", price: '$5', image: 'https://hc.com.vn/i/ecommerce/media/GS.007152_FEATURE_74775.jpg' },
 ]
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles()
 
     return (
@@ -15,7 +15,7 @@ const Products = () => {
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
